bb2729b8b04277f3ab367dae970b97bbdd4660e7
Justin Phillips <justinphil02@yahoo.com>
2019-12-17 04:11:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,7 @@ def self.create_table
   name TEXT,
   grade INTEGER
   );"
-  
+
   DB[:conn].execute(sql)
 end
 
@@ -32,6 +32,6 @@ def save
   sql = "INSERT INTO students (name, grade) VALUES (?,?);"
   DB[:conn].execute(sql, self.name, self.grade)
   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-end 
+end
 
 end
